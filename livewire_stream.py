c75515769ec2bc08f13ae6097bd71d9e21873506
Keoni Mahelona <keoni@kmahelona.net>
2022-08-07 15:55:07 +1200

    simplify and use right ips

--- livewire_stream.py
+++ livewire_stream.py
@@ -1,27 +1,10 @@
 #!/usr/bin/python
 import sys
 import os
-import socket
 import platform
-import struct
-
+import ipaddress
 
 BASE_AXIA_IP = 0xEFC00000  # 239.192.0.0 [in hex]
-
-
-def ipToDecimal(originalIp):
-    """ Takes a standard dotted-quad IP Address and returns it as an integer """
-
-    ipStruct = socket.inet_pton(socket.AF_INET, originalIp)
-    (ipDecimal, ) = struct.unpack(">L", ipStruct)
-    return ipDecimal
-
-
-def decimalToIp(originalDecimal):
-    """ Takes a integer-based IP Address and returns it as the standard dotted-quad format """
-
-    ipStruct = struct.pack(">L", originalDecimal)
-    return socket.inet_ntoa(ipStruct)
 
 
 platform = platform.platform()
@@ -40,10 +23,11 @@ if len(sys.argv) != 2:
     sys.exit(1)
 else:
     # Axia channel number + base IP (239.192.0.0 [in hex])
-    MULTI_CAST_ADDRESS = (
+    MULTI_CAST_ADDRESS = ipaddress.ip_address(
         int(sys.argv[1]) +
         BASE_AXIA_IP
-    )
+    ).__str__()
+
     print(
         RTPDUMP_BIN + " -F payload " + MULTI_CAST_ADDRESS + "/5004 | " +
         PLAY_BIN + " -c 2 -r 48000 -b 24 -e signed-integer  -B -t raw -"
