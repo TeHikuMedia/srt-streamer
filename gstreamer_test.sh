0f835d636dccfef557c166063c5f04b860b33f86
Keoni Mahelona <keoni@kmahelona.net>
2022-08-10 08:56:21 +1200

    gstreamer

---
+++ gstreamer_test.sh
@@ -0,0 +1,13 @@
+#/usr/bin/bash
+
+cd /home/tehiku/srt-streamer/
+
+source .env
+MULTICAST_IP_ADDR=$(python3 calc_ip.py $AXIA_PORT)
+AUDIO_UDP_PORT=5004
+
+gst-launch-0.10 udpsrc multicast-group=$MULTICAST_IP_ADDR auto-multicast=true port=$AUDIO_UDP_PORT \
+caps="application/x-rtp, media=(string)audio, clock-rate=(int)48000, encoding-name=(string)L24, \
+encoding-params=(string)1, payload=(int)96, channels=(int)2" \
+! rtpL24depay ! audioconvert ! vorbisenc ! oggmux \
+! shout2send mount=/$ICE_MOUNT port=$ICE_PORT username=$ICE_USER password=$ICE_PASS ip=$ICE_URL
