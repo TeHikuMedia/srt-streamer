485be931a6b78a82a38eab6047ae6aa6b22e0aaf
Keoni Mahelona <keoni@kmahelona.net>
2022-08-10 10:09:38 +1200

    wave intermediate

--- gstreamer_test.sh
+++ gstreamer_test.sh
@@ -10,5 +10,6 @@ gst-launch-1.0 -e --gst-debug-level=2 udpsrc address=$MULTICAST_IP_ADDR port=$AU
 caps="application/x-rtp, media=(string)audio, clock-rate=(int)48000, encoding-name=(string)L24, \
 encoding-params=(string)1, payload=(int)96, channels=(int)2" \
 buffer-size=96 \
-! rtpL24depay ! audioconvert ! audio/x-raw,format=S8,channels=2 ! audioconvert ! vorbisenc ! oggmux \
+! rtpL24depay ! audioconvert ! wavenc ! wavparse \
+! audioconvert ! queue ! vorbisenc ! oggmux \
 ! shout2send mount=/$ICE_MOUNT port=$ICE_PORT username=$ICE_USER password=$ICE_PASS ip=$ICE_URL
