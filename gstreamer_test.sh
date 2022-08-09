3e2eb0a9cdca5cecc48535f594f9d294c9535889
Keoni Mahelona <keoni@kmahelona.net>
2022-08-10 09:42:12 +1200

    buffer

--- gstreamer_test.sh
+++ gstreamer_test.sh
@@ -9,5 +9,6 @@ AUDIO_UDP_PORT=5004
 gst-launch-1.0 -e --gst-debug-level=2 udpsrc address=$MULTICAST_IP_ADDR port=$AUDIO_UDP_PORT  multicast-iface=eno2 \
 caps="application/x-rtp, media=(string)audio, clock-rate=(int)48000, encoding-name=(string)L24, \
 encoding-params=(string)1, payload=(int)96, channels=(int)2" \
+buffer-size=9600 \
 ! rtpL24depay ! audioconvert ! vorbisenc ! oggmux \
 ! shout2send mount=/$ICE_MOUNT port=$ICE_PORT username=$ICE_USER password=$ICE_PASS ip=$ICE_URL
