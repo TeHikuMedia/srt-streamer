1b02cc095ce9acdb9ddca4d39c7c3bf33f521a90
Keoni Mahelona <keoni@kmahelona.net>
2022-08-10 10:59:59 +1200

    add ave

--- gstreamer_test.sh
+++ gstreamer_test.sh
@@ -12,5 +12,8 @@ encoding-params=(string)1, payload=(int)96, channels=(int)2" \
 buffer-size=96 \
 ! rtpL24depay ! audioconvert
 ! audio/x-raw-float, width=32, channels=2, rate=480000, signed=true \
+! audioconvert
+! wavenc
+! wavparse
 ! vorbisenc ! oggmux \
 ! shout2send mount=/$ICE_MNT port=$ICE_PORT username=$ICE_USER password=$ICE_PASS ip=$ICE_URL
