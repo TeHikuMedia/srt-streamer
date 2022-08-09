fd9c4ee4b43eaeb02a7ab12bd1bb72c80e3f6ab7
Keoni Mahelona <keoni@kmahelona.net>
2022-08-10 09:37:22 +1200

    debug

--- gstreamer_test.sh
+++ gstreamer_test.sh
@@ -6,7 +6,7 @@ source .env
 MULTICAST_IP_ADDR=$(python3 calc_ip.py $AXIA_PORT)
 AUDIO_UDP_PORT=5004
 
-gst-launch-1.0 udpsrc address=$MULTICAST_IP_ADDR port=$AUDIO_UDP_PORT \
+gst-launch-1.0 -e --gst-debug-level=2 udpsrc address=$MULTICAST_IP_ADDR port=$AUDIO_UDP_PORT  multicast-iface=eno2 \
 caps="application/x-rtp, media=(string)audio, clock-rate=(int)48000, encoding-name=(string)L24, \
 encoding-params=(string)1, payload=(int)96, channels=(int)2" \
 ! rtpL24depay ! audioconvert ! vorbisenc ! oggmux \
