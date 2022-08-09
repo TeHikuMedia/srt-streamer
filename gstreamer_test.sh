7a27e509198077e1c5b9c57bc5bb39c7136e6db4
Keoni Mahelona <keoni@kmahelona.net>
2022-08-10 10:52:52 +1200

    fix mout

--- gstreamer_test.sh
+++ gstreamer_test.sh
@@ -10,6 +10,7 @@ gst-launch-1.0 -e --gst-debug-level=2 udpsrc address=$MULTICAST_IP_ADDR port=$AU
 caps="application/x-rtp, media=(string)audio, clock-rate=(int)48000, encoding-name=(string)L24, \
 encoding-params=(string)1, payload=(int)96, channels=(int)2" \
 buffer-size=96 \
-! rtpL24depay ! audioconvert ! wavenc ! wavparse \
-! audioconvert ! queue ! vorbisenc ! oggmux \
-! shout2send mount=/$ICE_MOUNT port=$ICE_PORT username=$ICE_USER password=$ICE_PASS ip=$ICE_URL
+! rtpL24depay ! audioconvert
+! audio/x-raw-float, width=32, channels=2, rate=16000, endianness=1234, signed=true \
+! vorbisenc ! oggmux \
+! shout2send mount=/$ICE_MNT port=$ICE_PORT username=$ICE_USER password=$ICE_PASS ip=$ICE_URL
