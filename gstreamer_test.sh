8f36d1744b5ad1f9175147d8295c617f259b577f
Keoni Mahelona <keoni@kmahelona.net>
2022-08-10 15:53:08 +1200

    skip bytes

--- gstreamer_test.sh
+++ gstreamer_test.sh
@@ -7,6 +7,7 @@ MULTICAST_IP_ADDR=$(python3 calc_ip.py $AXIA_PORT)
 AUDIO_UDP_PORT=5004
 
 gst-launch-1.0 -e --gst-debug-level=2 udpsrc address=$MULTICAST_IP_ADDR port=$AUDIO_UDP_PORT  multicast-iface=eno2 \
+skip-first-bytes=78 \
 caps="application/x-rtp, media=(string)audio, clock-rate=(int)48000, encoding-name=(string)L24, \
 encoding-params=(string)1, payload=(int)96, channels=(int)2" \
 #buffer-size=72 \
@@ -15,3 +16,5 @@ encoding-params=(string)1, payload=(int)96, channels=(int)2" \
 ! audio/x-raw, format=F32LE, layout=interleaved, channels=2, rate=480000, channel-mask=0x0000000000000003 \
 ! vorbisenc ! oggmux \
 ! shout2send mount=/$ICE_MNT port=$ICE_PORT username=$ICE_USER password=$ICE_PASS ip=$ICE_URL
+
+#78
