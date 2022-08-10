99d305f1c08acf6407d0b4479fa4f22532a94a87
Keoni Mahelona <keoni@kmahelona.net>
2022-08-10 16:52:49 +1200

    add multi

--- relay.sh
+++ relay.sh
@@ -13,6 +13,6 @@ skip-first-bytes=78 buffer-size=0 \
 caps="application/x-rtp, media=(string)audio, clock-rate=(int)48000, encoding-name=(string)L24, \
 encoding-params=(string)1, payload=(int)96, channels=(int)2" \
 ! queue
-! udpsink address=$SEND_ADDR port=$AUDIO_UDP_PORT  multicast-iface=eno2
+! udpsink address=$SEND_ADDR port=$AUDIO_UDP_PORT auto-multicast=true multicast-iface=eno2
 
 #78
